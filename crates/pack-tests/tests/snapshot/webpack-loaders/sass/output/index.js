c312315db81218c255922f8861e125a28e87a32c
xusd320 <xusd320@gmail.com>
2025-07-18 18:44:03 +0800

    test: add test cases

---
+++ crates/pack-tests/tests/snapshot/webpack-loaders/sass/output/index.js
@@ -0,0 +1,6 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    typeof document === "object" ? document.currentScript : undefined,
+    {},
+    {"otherChunks":["crates_pack-tests_tests_snapshot_webpack-loaders_sass_input_style_sass_80390d50.css","crates_pack-tests_tests_snapshot_webpack-loaders_sass_input_index_ts_3f4125a9.js"],"runtimeModuleIds":[89]}
+]);
+// Dummy runtime
