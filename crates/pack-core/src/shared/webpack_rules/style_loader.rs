c2e45b8d00f45838bbda16350721e1fdd4348c9e
xusd320 <xusd320@gmail.com>
2025-07-21 13:24:16 +0800

    feat(pack): let internal dependencies of @utoo/pack be pinned (#2061)

--- crates/pack-core/src/shared/webpack_rules/style_loader.rs
+++ crates/pack-core/src/shared/webpack_rules/style_loader.rs
@@ -1,36 +1,18 @@
 use std::mem::take;
 
 use anyhow::{Result, bail};
-use serde_json::Value as JsonValue;
+use turbo_rcstr::rcstr;
+use turbo_tasks_fs::FileSystemPath;
 use turbopack_node::transforms::webpack::WebpackLoaderItem;
 
 use turbo_tasks::{ResolvedVc, Vc};
 use turbopack::module_options::{LoaderRuleItem, OptionWebpackRules, WebpackRules};
 
-use crate::config::OptionalJsonValue;
-
-fn style_loader(inline_css: &JsonValue) -> Result<WebpackLoaderItem> {
-    let Some(inline_css) = inline_css.as_object().cloned() else {
-        bail!("inline_css must be an object");
-    };
-
-    let style_loader = WebpackLoaderItem {
-        loader: "@utoo/style-loader".into(),
-        options: take(
-            serde_json::json!({
-                "insert": inline_css.get("insert"),
-                "injectType": inline_css.get("injectType"),
-            })
-            .as_object_mut()
-            .unwrap(),
-        ),
-    };
-
-    Ok(style_loader)
-}
+use crate::{config::OptionalJsonValue, import_map::get_utoopack_dependency_package};
 
 #[turbo_tasks::function]
 pub async fn maybe_add_style_loader(
+    project_path: FileSystemPath,
     inline_css: Vc<OptionalJsonValue>,
     webpack_rules: Option<Vc<WebpackRules>>,
 ) -> Result<Vc<OptionWebpackRules>> {
@@ -49,7 +31,27 @@ pub async fn maybe_add_style_loader(
 
     for (pattern, rename) in [("*.css", ".js")] {
         let rule = rules.get_mut(pattern);
-        let style_loader = style_loader(inline_css)?;
+
+        let Some(inline_css) = inline_css.as_object().cloned() else {
+            bail!("inline_css must be an object");
+        };
+
+        let style_loader = WebpackLoaderItem {
+            loader: get_utoopack_dependency_package(
+                project_path.clone(),
+                rcstr!("@utoo/style-loader"),
+            )
+            .owned()
+            .await?,
+            options: take(
+                serde_json::json!({
+                    "insert": inline_css.get("insert"),
+                    "injectType": inline_css.get("injectType"),
+                })
+                .as_object_mut()
+                .unwrap(),
+            ),
+        };
 
         if let Some(rule) = rule {
             // Without `as`, loader result would be JS code, so we don't want to apply
