6519887a1be2ad9a130062f5fa514b01207e90f6
zoomdong <1344492820@qq.com>
2025-07-24 17:28:28 +0800

    chore: fmt

--- crates/pack-core/src/import_map.rs
+++ crates/pack-core/src/import_map.rs
@@ -26,9 +26,12 @@ pub fn mdx_import_source_file() -> RcStr {
 }
 
 #[turbo_tasks::function]
-pub async fn get_postcss_package_mapping(project_path: FileSystemPath) -> Result<Vc<ImportMapping>> {
+pub async fn get_postcss_package_mapping(
+    project_path: FileSystemPath,
+) -> Result<Vc<ImportMapping>> {
     Ok(ImportMapping::Alternatives(vec![
-        ImportMapping::PrimaryAlternative("postcss".into(), Some(project_path.clone())).resolved_cell(),
+        ImportMapping::PrimaryAlternative("postcss".into(), Some(project_path.clone()))
+            .resolved_cell(),
     ])
     .cell())
 }
