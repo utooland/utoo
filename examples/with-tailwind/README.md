2ce22b2c8b8c4cbd873e618a06aa0e19a1dbf07c
zoomdong <1344492820@qq.com>
2025-07-24 19:20:49 +0800

    feat(pack): enable postcss and add tailwind example (#2078)

---
+++ examples/with-tailwind/README.md
@@ -0,0 +1,7 @@
+## with-tailwindcss
+
+A example from muse, the following featues need to enable:
+
+- postcss & postcss-loader
+- tailwindcss
+- style-loader
---
+++ examples/with-tailwind/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './main.css';
+
+function App() {
+    return <div className="text-3xl font-bold underline">TailwindCSS Example by Utoopack.</div>
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
