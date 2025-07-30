718b3129cdd22ad33cd1fba27fc06c6806d5276c
zoomdong <1344492820@qq.com>
2025-07-30 09:14:49 +0800

    feat(pack): support regex for matching webpack loaders (#2088)

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { foo } from './foo.ts';
 import dataText from './test.txt';
+import Person from '../static/person.svg';
 
 export function App() {
-  return <h1>App {foo} - HMR Test by {dataText}</h1>;
+  return <>
+    <h1>App {foo} - HMR Test by {dataText}</h1>
+    <Person />
+  </>;
 }
