97fc0779adfa2a6a5fa412aba38094de3de8557d
xusd320 <xusd320@gmail.com>
2025-08-12 14:16:36 +0800

    feat(wasm): support utoopack (#2111)

---
+++ crates/utoo-wasm/src/pack.rs
@@ -0,0 +1,135 @@
+use std::{sync::Arc, time::Instant};
+
+use anyhow::Result;
+use pack_api::{
+    entrypoint::{get_all_written_entrypoints_with_issues_operation, EntrypointsWithIssues},
+    project::{ProjectContainer, ProjectOptions, WatchOptions},
+};
+use serde::{Deserialize, Serialize};
+use turbo_rcstr::RcStr;
+use turbo_tasks::{ResolvedVc, TurboTasks};
+use turbo_tasks_backend::{
+    noop_backing_storage, BackendOptions, NoopBackingStorage, TurboTasksBackend,
+};
+use turbo_tasks_malloc::TurboMalloc;
+use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
+
+pub fn register() {
+    pack_api::register();
+    include!(concat!(env!("OUT_DIR"), "/register.rs"));
+}
+
+#[derive(Debug, Default, Serialize, Deserialize, Clone, PartialEq)]
+#[wasm_bindgen]
+pub struct PartialProjectOptions {
+    #[wasm_bindgen(getter_with_clone)]
+    pub project_path: String,
+
+    #[wasm_bindgen(getter_with_clone)]
+    pub config: Option<String>,
+}
+
+#[wasm_bindgen]
+pub async fn build(partial_options: PartialProjectOptions) -> std::result::Result<(), JsValue> {
+    let project_path: RcStr = partial_options.project_path.into();
+    let config: RcStr = partial_options.config.unwrap_or("{}".to_string()).into();
+    let options = ProjectOptions {
+        root_path: project_path.clone(),
+        project_path: project_path.clone(),
+        config,
+        process_env: Default::default(),
+        define_env: Default::default(),
+        watch: WatchOptions {
+            enable: false,
+            ..Default::default()
+        },
+        dev: false,
+        build_id: project_path.clone(),
+    };
+    build_internal(options)
+        .await
+        .map_err(|e| JsValue::from_str(&e.to_string()))
+}
+
+async fn build_internal(options: ProjectOptions) -> Result<()> {
+    tracing::info!("bundling",);
+
+    let start = Instant::now();
+
+    let (turbo_tasks, project_container) = initialize_project_container(options).await?;
+
+    let (entrypoints, _issues, _diagnostics) = turbo_tasks
+        .run_once(async move {
+            let entrypoints_with_issues_op =
+                get_all_written_entrypoints_with_issues_operation(project_container);
+
+            let EntrypointsWithIssues {
+                entrypoints,
+                issues,
+                diagnostics,
+                effects,
+            } = &*entrypoints_with_issues_op
+                .read_strongly_consistent()
+                .await?;
+            effects.apply().await?;
+
+            Ok((entrypoints.clone(), issues.clone(), diagnostics.clone()))
+        })
+        .await?;
+
+    tracing::info!("all project entrypoints wrote to disk.");
+
+    tracing::info!(
+        "pack tasks with {} apps {} libraries finished in {:?}",
+        entrypoints
+            .apps
+            .as_ref()
+            .map(|apps| apps.0.len())
+            .unwrap_or_default(),
+        entrypoints
+            .libraries
+            .as_ref()
+            .map(|libraries| libraries.0.len())
+            .unwrap_or_default(),
+        start.elapsed()
+    );
+
+    let memory = TurboMalloc::memory_usage();
+    tracing::info!("memory usage: {} MiB", memory / 1024 / 1024);
+
+    let start = Instant::now();
+    drop(turbo_tasks);
+
+    tracing::info!("drop {:?}", start.elapsed());
+
+    Ok(())
+}
+
+async fn initialize_project_container(
+    options: ProjectOptions,
+) -> Result<
+    (
+        Arc<TurboTasks<TurboTasksBackend<NoopBackingStorage>>>,
+        ResolvedVc<ProjectContainer>,
+    ),
+    anyhow::Error,
+> {
+    let turbo_tasks = TurboTasks::new(TurboTasksBackend::new(
+        BackendOptions {
+            dependency_tracking: true,
+            storage_mode: None,
+            ..Default::default()
+        },
+        noop_backing_storage(),
+    ));
+    let project_container = turbo_tasks
+        .run_once(async move {
+            let project_container = ProjectContainer::new("utoopack-web".into(), false);
+            let project_container = project_container.to_resolved().await?;
+            project_container.initialize(options).await?;
+            Ok(project_container)
+        })
+        .await?;
+
+    Ok((turbo_tasks, project_container))
+}
---
+++ crates/utoo-wasm/src/project.rs
@@ -0,0 +1,90 @@
+use opfs_project::cwd::{self, get_cwd};
+use opfs_project::package_manager;
+use opfs_project::{opfs, DirEntry};
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsValue;
+
+use crate::pack::{build, PartialProjectOptions};
+
+#[wasm_bindgen]
+pub struct Project {}
+
+#[wasm_bindgen]
+impl Project {
+    #[wasm_bindgen(constructor)]
+    pub fn new(cwd: String) -> Project {
+        cwd::set_cwd(cwd.clone());
+        Project {}
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn cwd(&self) -> String {
+        cwd::get_cwd()
+    }
+
+    #[wasm_bindgen]
+    pub async fn install(&self, package_lock: String) -> Result<(), JsValue> {
+        package_manager::install_deps(&package_lock)
+            .await
+            .map_err(|e| JsValue::from_str(&format!("install_deps error: {e}")))?;
+        Ok(())
+    }
+
+    #[wasm_bindgen]
+    pub async fn build(&self) -> Result<(), JsValue> {
+        let config = self.read_file("./project_options.json").await.ok();
+        let partial_options = PartialProjectOptions {
+            project_path: ".".to_string(),
+            config,
+        };
+        build(partial_options).await
+    }
+
+    #[wasm_bindgen(js_name = readFile)]
+    pub async fn read_file(&self, path: &str) -> Result<String, JsValue> {
+        let content = opfs::read_with_fuse_link(path)
+            .await
+            .map_err(|e| JsValue::from_str(&format!("read error: {e}")))?;
+        Ok(unsafe { String::from_utf8_unchecked(content) })
+    }
+
+    #[wasm_bindgen(js_name = writeFile)]
+    pub async fn write_file(&self, path: &str, content: &str) -> Result<(), JsValue> {
+        opfs::write(path, content)
+            .await
+            .map_err(|e| JsValue::from_str(&format!("write error: {e}")))?;
+        Ok(())
+    }
+
+    #[wasm_bindgen(js_name = readDir)]
+    pub async fn read_dir(&self, path: &str) -> Result<Vec<DirEntry>, JsValue> {
+        let res = opfs::read_dir(path)
+            .await
+            .map_err(|e| JsValue::from_str(&format!("read_dir error: {e}")))?;
+        Ok(res)
+    }
+
+    #[wasm_bindgen(js_name = createDir)]
+    pub async fn create_dir(&self, path: &str) -> Result<(), JsValue> {
+        opfs::create_dir(path)
+            .await
+            .map_err(|e| JsValue::from_str(&format!("create_dir error: {e}")))?;
+        Ok(())
+    }
+
+    #[wasm_bindgen(js_name = createDirAll)]
+    pub async fn create_dir_all(&self, path: &str) -> Result<(), JsValue> {
+        opfs::create_dir_all(path)
+            .await
+            .map_err(|e| JsValue::from_str(&format!("create_dir_all error: {e}")))?;
+        Ok(())
+    }
+
+    #[wasm_bindgen(js_name = copyFile)]
+    pub async fn copy_file(&self, src: &str, dst: &str) -> Result<(), JsValue> {
+        opfs::copy(src, dst)
+            .await
+            .map_err(|e| JsValue::from_str(&format!("copy error: {e}")))?;
+        Ok(())
+    }
+}
