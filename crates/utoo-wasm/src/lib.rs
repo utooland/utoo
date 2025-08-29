fe11b8636a50c2a55ee3cac3ff0e7417789b23d7
xusd320 <xusd320@gmail.com>
2025-08-29 19:08:07 +0800

    perf(wasm): avoid too many console.log (#2158)

--- crates/utoo-wasm/src/lib.rs
+++ crates/utoo-wasm/src/lib.rs
@@ -4,7 +4,6 @@ extern crate console_error_panic_hook;
 
 use std::panic;
 
-use pack_core::tracing_presets::TRACING_OVERVIEW_TARGETS;
 use tracing_subscriber::{
     fmt::{
         self,
@@ -22,16 +21,14 @@ use wasm_bindgen::prelude::wasm_bindgen;
 #[cfg(feature = "utoo-pack")]
 pub(crate) mod pack;
 
+#[cfg(feature = "utoo-pack")]
 mod opfs_offload;
 mod project;
 pub(crate) mod tokio_runtime;
 pub use project::Project;
 
 #[wasm_bindgen(start)]
-#[cfg(feature = "utoo-pack")]
 fn init_pack() {
-    use crate::opfs_offload::OpfsOffload;
-
     panic::set_hook(Box::new(console_error_panic_hook::hook));
 
     let fmt_layer = fmt::layer()
@@ -39,20 +36,23 @@ fn init_pack() {
         .with_span_events(FmtSpan::CLOSE)
         .with_writer(MakeWebConsoleWriter::new())
         .with_filter(EnvFilter::new({
-            let mut trace = TRACING_OVERVIEW_TARGETS
-                .iter()
-                // .filter(|_| true)
-                .cloned()
-                .collect::<Vec<&str>>();
-
-            trace.extend(["utoo_wasm=info"]);
-            trace.join(",")
+            let trace = vec!["pack_core=info", "pack_api=info", "utoo_wasm=info"];
+            [
+                trace,
+                // pack_core::tracing_presets::TRACING_OVERVIEW_TARGETS.to_vec(),
+            ]
+            .concat()
+            .join(",")
         }));
 
     registry().with(fmt_layer).init();
 
     #[cfg(feature = "utoo-pack")]
-    pack::register();
+    {
+        pack::register();
 
-    wasm_bindgen_futures::spawn_local(turbo_tasks_fs::wasm_fs_offload::server(OpfsOffload))
+        wasm_bindgen_futures::spawn_local(turbo_tasks_fs::wasm_fs_offload::server(
+            crate::opfs_offload::OpfsOffload,
+        ))
+    }
 }
