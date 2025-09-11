cdd7849a951dcfc3fcaf394aa48d019856d2f05f
xusd320 <xusd320@gmail.com>
2025-09-11 17:57:27 +0800

    perf(wasm): tokio worker threads num to 4 (#2190)

--- crates/utoo-wasm/src/tokio_runtime.rs
+++ crates/utoo-wasm/src/tokio_runtime.rs
@@ -17,10 +17,15 @@ pub fn init_tokio_runtime(worker_url: String) {
     TOKIO_RUNTIME.with(|ctx| {
         ctx.get_or_init(|| {
             runtime::Builder::new_multi_thread()
-                // .enable_time()
+                /* *
+                 * The default dlmalloc is a single global allocator,
+                 * it will block threads scheduling,
+                 * see https://web.dev/articles/scaling-multithreaded-webassembly-applications#heap_management_mallocfree
+                 */
+                .worker_threads(4)
                 .disable_lifo_slot()
                 .thread_name_fn(|| {
-                    static ATOMIC_ID: AtomicUsize = AtomicUsize::new(0);
+                    static ATOMIC_ID: AtomicUsize = AtomicUsize::new(1);
                     let id = ATOMIC_ID.fetch_add(1, Ordering::SeqCst);
                     format!("tokio-runtime-worker-{id}")
                 })
