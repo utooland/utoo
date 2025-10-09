cc68214f5510fe0e68aa36cf13013958cf0cdbf4
zoomdong <1344492820@qq.com>
2025-10-09 14:48:09 +0800

    chore: bump next.js & swc & rust toolchain

--- crates/pack-api/src/utils.rs
+++ crates/pack-api/src/utils.rs
@@ -73,7 +73,7 @@ pub async fn strongly_consistent_catch_collectables<R: VcValueType + Send>(
 }
 
 pub async fn get_issues<T: Send>(source: OperationVc<T>) -> Result<Arc<Vec<ReadRef<PlainIssue>>>> {
-    let issues = source.peek_issues().await?;
+    let issues = source.peek_issues();
     Ok(Arc::new(issues.get_plain_issues().await?))
 }
 
