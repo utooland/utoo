16d0856795ff95f6854802b6272ffd1b62e56f8c
zoomdong <1344492820@qq.com>
2025-10-24 10:55:51 +0800

    fix(pack): just ignore .d.ts file when bundle (#2263)

---
+++ crates/pack-tests/tests/snapshot/typescript/dts_export/output/crates_pack-tests_tests_snapshot_typescript_dts_export_input_index_ts_3420c2bb.js
@@ -0,0 +1,16 @@
+(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([typeof document === "object" ? document.currentScript : undefined,
+54, ((__turbopack_context__) => {
+"use strict";
+
+;
+__turbopack_context__.s([]);
+}),
+6, ((__turbopack_context__) => {
+"use strict";
+
+var __TURBOPACK__imported__module__54__ = __turbopack_context__.i(54);
+__turbopack_context__.s([]);
+}),
+]);
+
+//# sourceMappingURL=crates_pack-tests_tests_snapshot_typescript_dts_export_input_index_ts_3420c2bb.js.map
