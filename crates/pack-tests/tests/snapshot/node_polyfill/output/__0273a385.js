cb951cea0161004bf86ff3df90b8ee57af898d36
zoomdong <1344492820@qq.com>
2025-10-27 00:04:12 +0800

    chore: update feature-list

--- crates/pack-tests/tests/snapshot/node_polyfill/output/__0273a385.js
+++ crates/pack-tests/tests/snapshot/node_polyfill/output/__0273a385.js
@@ -5415,6 +5415,201 @@
     module.exports = t;
 })();
 }),
+"[@utoo/pack-runtime]/node-polyfills/process/browser.js [client] (ecmascript)", ((__turbopack_context__, module, exports) => {
+
+(function() {
+    var e = {
+        229: function(e) {
+            var t = e.exports = {};
+            var r;
+            var n;
+            function defaultSetTimout() {
+                throw new Error("setTimeout has not been defined");
+            }
+            function defaultClearTimeout() {
+                throw new Error("clearTimeout has not been defined");
+            }
+            (function() {
+                try {
+                    if (typeof setTimeout === "function") {
+                        r = setTimeout;
+                    } else {
+                        r = defaultSetTimout;
+                    }
+                } catch (e) {
+                    r = defaultSetTimout;
+                }
+                try {
+                    if (typeof clearTimeout === "function") {
+                        n = clearTimeout;
+                    } else {
+                        n = defaultClearTimeout;
+                    }
+                } catch (e) {
+                    n = defaultClearTimeout;
+                }
+            })();
+            function runTimeout(e) {
+                if (r === setTimeout) {
+                    return setTimeout(e, 0);
+                }
+                if ((r === defaultSetTimout || !r) && setTimeout) {
+                    r = setTimeout;
+                    return setTimeout(e, 0);
+                }
+                try {
+                    return r(e, 0);
+                } catch (t) {
+                    try {
+                        return r.call(null, e, 0);
+                    } catch (t) {
+                        return r.call(this, e, 0);
+                    }
+                }
+            }
+            function runClearTimeout(e) {
+                if (n === clearTimeout) {
+                    return clearTimeout(e);
+                }
+                if ((n === defaultClearTimeout || !n) && clearTimeout) {
+                    n = clearTimeout;
+                    return clearTimeout(e);
+                }
+                try {
+                    return n(e);
+                } catch (t) {
+                    try {
+                        return n.call(null, e);
+                    } catch (t) {
+                        return n.call(this, e);
+                    }
+                }
+            }
+            var i = [];
+            var o = false;
+            var u;
+            var a = -1;
+            function cleanUpNextTick() {
+                if (!o || !u) {
+                    return;
+                }
+                o = false;
+                if (u.length) {
+                    i = u.concat(i);
+                } else {
+                    a = -1;
+                }
+                if (i.length) {
+                    drainQueue();
+                }
+            }
+            function drainQueue() {
+                if (o) {
+                    return;
+                }
+                var e = runTimeout(cleanUpNextTick);
+                o = true;
+                var t = i.length;
+                while(t){
+                    u = i;
+                    i = [];
+                    while(++a < t){
+                        if (u) {
+                            u[a].run();
+                        }
+                    }
+                    a = -1;
+                    t = i.length;
+                }
+                u = null;
+                o = false;
+                runClearTimeout(e);
+            }
+            t.nextTick = function(e) {
+                var t = new Array(arguments.length - 1);
+                if (arguments.length > 1) {
+                    for(var r = 1; r < arguments.length; r++){
+                        t[r - 1] = arguments[r];
+                    }
+                }
+                i.push(new Item(e, t));
+                if (i.length === 1 && !o) {
+                    runTimeout(drainQueue);
+                }
+            };
+            function Item(e, t) {
+                this.fun = e;
+                this.array = t;
+            }
+            Item.prototype.run = function() {
+                this.fun.apply(null, this.array);
+            };
+            t.title = "browser";
+            t.browser = true;
+            t.env = {};
+            t.argv = [];
+            t.version = "";
+            t.versions = {};
+            function noop() {}
+            t.on = noop;
+            t.addListener = noop;
+            t.once = noop;
+            t.off = noop;
+            t.removeListener = noop;
+            t.removeAllListeners = noop;
+            t.emit = noop;
+            t.prependListener = noop;
+            t.prependOnceListener = noop;
+            t.listeners = function(e) {
+                return [];
+            };
+            t.binding = function(e) {
+                throw new Error("process.binding is not supported");
+            };
+            t.cwd = function() {
+                return "/";
+            };
+            t.chdir = function(e) {
+                throw new Error("process.chdir is not supported");
+            };
+            t.umask = function() {
+                return 0;
+            };
+        }
+    };
+    var t = {};
+    function __nccwpck_require__(r) {
+        var n = t[r];
+        if (n !== undefined) {
+            return n.exports;
+        }
+        var i = t[r] = {
+            exports: {}
+        };
+        var o = true;
+        try {
+            e[r](i, i.exports, __nccwpck_require__);
+            o = false;
+        } finally{
+            if (o) delete t[r];
+        }
+        return i.exports;
+    }
+    if (typeof __nccwpck_require__ !== "undefined") __nccwpck_require__.ab = ("TURBOPACK compile-time value", "/ROOT/node-polyfills/process") + "/";
+    var r = __nccwpck_require__(229);
+    module.exports = r;
+})();
+}),
+"[project]/crates/pack-tests/tests/snapshot/node_modules/browser-polyfill/index.js [client] (ecmascript)", ((__turbopack_context__) => {
+"use strict";
+
+const process = __turbopack_context__.r("[@utoo/pack-runtime]/node-polyfills/process/browser.js [client] (ecmascript)");
+;
+__turbopack_context__.s([
+    "process",
+    ()=>process
+]);
+}),
 "[@utoo/pack-runtime]/node-polyfills/events/events.js [client] (ecmascript)", ((__turbopack_context__, module, exports) => {
 
 (function() {
@@ -10410,13 +10605,15 @@
 
 var __TURBOPACK__imported__module__$5b40$utoo$2f$pack$2d$runtime$5d2f$node$2d$polyfills$2f$assert$2f$assert$2e$js__$5b$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[@utoo/pack-runtime]/node-polyfills/assert/assert.js [client] (ecmascript)");
 var __TURBOPACK__imported__module__$5b40$utoo$2f$pack$2d$runtime$5d2f$node$2d$polyfills$2f$buffer$2f$index$2e$js__$5b$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[@utoo/pack-runtime]/node-polyfills/buffer/index.js [client] (ecmascript)");
+var __TURBOPACK__imported__module__$5b$project$5d2f$crates$2f$pack$2d$tests$2f$tests$2f$snapshot$2f$node_modules$2f$browser$2d$polyfill$2f$index$2e$js__$5b$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/crates/pack-tests/tests/snapshot/node_modules/browser-polyfill/index.js [client] (ecmascript)");
 ;
 ;
 const stream = __turbopack_context__.r("[@utoo/pack-runtime]/node-polyfills/stream-browserify/index.js [client] (ecmascript)");
-console.log(__TURBOPACK__imported__module__$5b40$utoo$2f$pack$2d$runtime$5d2f$node$2d$polyfills$2f$assert$2f$assert$2e$js__$5b$client$5d$__$28$ecmascript$29$__["default"], __TURBOPACK__imported__module__$5b40$utoo$2f$pack$2d$runtime$5d2f$node$2d$polyfills$2f$buffer$2f$index$2e$js__$5b$client$5d$__$28$ecmascript$29$__["default"]);
+;
+console.log(__TURBOPACK__imported__module__$5b40$utoo$2f$pack$2d$runtime$5d2f$node$2d$polyfills$2f$assert$2f$assert$2e$js__$5b$client$5d$__$28$ecmascript$29$__["default"], __TURBOPACK__imported__module__$5b40$utoo$2f$pack$2d$runtime$5d2f$node$2d$polyfills$2f$buffer$2f$index$2e$js__$5b$client$5d$__$28$ecmascript$29$__["default"], __TURBOPACK__imported__module__$5b$project$5d2f$crates$2f$pack$2d$tests$2f$tests$2f$snapshot$2f$node_modules$2f$browser$2d$polyfill$2f$index$2e$js__$5b$client$5d$__$28$ecmascript$29$__["process"]);
 console.log(stream);
 __turbopack_context__.s([]);
 }),
 ]);
 
-//# sourceMappingURL=__1f58485b.js.map
+//# sourceMappingURL=__0273a385.js.map
--- crates/pack-tests/tests/snapshot/node_polyfill/output/main.js
+++ crates/pack-tests/tests/snapshot/node_polyfill/output/main.js
@@ -1,5 +1,5 @@
 (globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([
     typeof document === "object" ? document.currentScript : undefined,
-    {"otherChunks":["__1f58485b.js"],"runtimeModuleIds":["[project]/crates/pack-tests/tests/snapshot/node_polyfill/input/index.ts [client] (ecmascript)"]}
+    {"otherChunks":["__0273a385.js"],"runtimeModuleIds":["[project]/crates/pack-tests/tests/snapshot/node_polyfill/input/index.ts [client] (ecmascript)"]}
 ]);
 // Dummy runtime
