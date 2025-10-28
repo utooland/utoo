b597f794c9d10ecfcb04dfb4e638b1201af2ecc7
zoomdong <1344492820@qq.com>
2025-10-28 14:01:51 +0800

    fix(pack): make global pseudo elements work without parse error (#2270)

---
+++ crates/pack-tests/tests/snapshot/style/global_css/output/main.js
@@ -0,0 +1,5 @@
+(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([
+    typeof document === "object" ? document.currentScript : undefined,
+    {"otherChunks":["crates_pack-tests_tests_snapshot_style_global_css_input_index_less_c00ab709.css","crates_pack-tests_tests_snapshot_style_global_css_input_index_ed5cc3e3.js"],"runtimeModuleIds":[68]}
+]);
+// Dummy runtime
