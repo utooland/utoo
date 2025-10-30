a4ca712d4429c8d561136f293522f5b8b1afbab3
zoomdong <1344492820@qq.com>
2025-10-30 13:51:56 +0800

    chore: format Cargo.toml to satisfy taplo

--- crates/pack-tests/tests/snapshot/style/global_scoped/output/main.js
+++ crates/pack-tests/tests/snapshot/style/global_scoped/output/main.js
@@ -1,5 +1,9 @@
 (globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([
     typeof document === "object" ? document.currentScript : undefined,
+<<<<<<< HEAD
     {"otherChunks":["crates_pack-tests_tests_snapshot_style_global_scoped_input_2c24b8f7.css","crates_pack-tests_tests_snapshot_style_global_scoped_input_d18f6c34.js"],"runtimeModuleIds":[98]}
+=======
+    {"otherChunks":["crates_pack-tests_tests_snapshot_style_global_scoped_input_c2ad8a9a.css","crates_pack-tests_tests_snapshot_style_global_scoped_input_cf8d3b45.js"],"runtimeModuleIds":[98]}
+>>>>>>> 2d9a820b (chore: format Cargo.toml to satisfy taplo)
 ]);
 // Dummy runtime
