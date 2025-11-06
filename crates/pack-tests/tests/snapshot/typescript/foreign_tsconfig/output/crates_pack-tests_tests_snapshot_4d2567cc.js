951041aeee70038dab906975e2327ffcf3afe97b
zoomdong <1344492820@qq.com>
2025-11-06 20:43:42 +0800

    fix(pack): enable ts & jsx transform for node_modules package (#2300)

--- crates/pack-tests/tests/snapshot/typescript/foreign_tsconfig/output/crates_pack-tests_tests_snapshot_4d2567cc.js
+++ crates/pack-tests/tests/snapshot/typescript/foreign_tsconfig/output/crates_pack-tests_tests_snapshot_4d2567cc.js
@@ -9,16 +9,27 @@ __turbopack_context__.s([
     foo
 ]);
 }),
+13, ((__turbopack_context__) => {
+"use strict";
+
+const prop = "prop";
+__turbopack_context__.s([
+    "prop",
+    0,
+    prop
+]);
+}),
 28, ((__turbopack_context__) => {
 "use strict";
 
 var __TURBOPACK__imported__module__81__ = __turbopack_context__.i(81);
+var __TURBOPACK__imported__module__13__ = __turbopack_context__.i(13);
 ;
 ;
 console.log('foo', __TURBOPACK__imported__module__81__["foo"]);
-console.log('prop', void 0);
+console.log('prop', __TURBOPACK__imported__module__13__["prop"]);
 __turbopack_context__.s([]);
 }),
 ]);
 
-//# sourceMappingURL=crates_pack-tests_tests_snapshot_86c66f3f.js.map
+//# sourceMappingURL=crates_pack-tests_tests_snapshot_4d2567cc.js.map
--- crates/pack-tests/tests/snapshot/typescript/foreign_tsconfig/output/main.js
+++ crates/pack-tests/tests/snapshot/typescript/foreign_tsconfig/output/main.js
@@ -1,5 +1,5 @@
 (globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([
     typeof document === "object" ? document.currentScript : undefined,
-    {"otherChunks":["crates_pack-tests_tests_snapshot_86c66f3f.js"],"runtimeModuleIds":[28]}
+    {"otherChunks":["crates_pack-tests_tests_snapshot_4d2567cc.js"],"runtimeModuleIds":[28]}
 ]);
 // Dummy runtime
