15e7c946c2c39039f3586e9157adf574b00e96a7
zoomdong <1344492820@qq.com>
2025-11-07 10:57:32 +0800

    fix(pack): ignore invalid pseudo selector error & unpure selector error (#2303)

---
+++ crates/pack-tests/tests/snapshot/style/invalid_pseudo_class/output/main.js
@@ -0,0 +1,5 @@
+(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([
+    typeof document === "object" ? document.currentScript : undefined,
+    {"otherChunks":["38c48_pack-tests_tests_snapshot_style_invalid_pseudo_class_input_index_less_e8c3d1e8.css","crates_pack-tests_tests_snapshot_style_invalid_pseudo_class_input_cfc79b22.js"],"runtimeModuleIds":[13]}
+]);
+// Dummy runtime
