c3aa96dd1d64a8719f1fcbe3593b077dbe2c8c0b
zoomdong <1344492820@qq.com>
2025-11-10 10:16:29 +0800

    chore: add test case for browser filed (#2305)

--- crates/pack-tests/tests/snapshot/basic/alias/input/index.ts
+++ crates/pack-tests/tests/snapshot/basic/alias/input/index.ts
@@ -2,8 +2,9 @@ import { foo } from 'hello-a';
 import { aliasPkg, aliasA } from "alias-pkg";
 import { bar } from '@@/a';
 import { aliasB } from '@@/b';
+import { a as browserslistA } from 'browserslist';
 
-console.log(foo, aliasPkg, bar);
+console.log(browserslistA, foo, aliasPkg, bar);
 
 console.log('a from alias-pkg', aliasA);
 console.log('b from alias-pkg', aliasB);
--- crates/pack-tests/tests/snapshot/basic/alias/output/main.js
+++ crates/pack-tests/tests/snapshot/basic/alias/output/main.js
@@ -1,5 +1,5 @@
 (globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([
     typeof document === "object" ? document.currentScript : undefined,
-    {"otherChunks":["crates_pack-tests_tests_snapshot_d4ef97b5.js"],"runtimeModuleIds":[70]}
+    {"otherChunks":["crates_pack-tests_tests_snapshot_2c1a2c5e.js"],"runtimeModuleIds":[70]}
 ]);
 // Dummy runtime
