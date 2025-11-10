f74da8aebff33092bb23ff7a7c590a31cf29edd5
zoomdong <1344492820@qq.com>
2025-11-10 14:42:35 +0800

    chore(pack): add more test case for modularizeImports (#2306)

--- crates/pack-tests/tests/snapshot/optimization/modularize_imports/output/main.js
+++
@@ -1,5 +0,0 @@
-(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([
-    typeof document === "object" ? document.currentScript : undefined,
-    {"otherChunks":["crates_pack-tests_tests_snapshot_b5b07607.js"],"runtimeModuleIds":["[project]/crates/pack-tests/tests/snapshot/optimization/modularize_imports/input/index.js [client] (ecmascript)"]}
-]);
-// Dummy runtime
