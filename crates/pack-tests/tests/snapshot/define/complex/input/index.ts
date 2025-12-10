f775c43b09537e2928805c8147da3ef8f79b4ac6
fireairforce <1344492820@qq.com>
2025-12-10 11:06:22 +0800

    fix(pack): define value too big to make define not work

---
+++ crates/pack-tests/tests/snapshot/define/complex/input/index.ts
@@ -0,0 +1,2 @@
+console.log(RoutePathEnum);
+console.log(RoutePathEnum.TEST20);
---
+++ crates/pack-tests/tests/snapshot/define/complex/output/main.js
@@ -0,0 +1,5 @@
+(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([
+    typeof document === "object" ? document.currentScript : undefined,
+    {"otherChunks":["crates_pack-tests_tests_snapshot_define_complex_input_index_ts_d888f878.js"],"runtimeModuleIds":[77]}
+]);
+// Dummy runtime
