f5ccfb713e054335ccc4d2c29fedbf0038e1d0eb
zoomdong <1344492820@qq.com>
2025-12-15 00:57:18 +0800

    chore: use small wasm file at test case

--- crates/pack-tests/tests/snapshot/static-asssets/wasm/output/main.js
+++ crates/pack-tests/tests/snapshot/static-asssets/wasm/output/main.js
@@ -1,5 +1,5 @@
 (globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([
     typeof document === "object" ? document.currentScript : undefined,
-    {"otherChunks":["crates_pack-tests_tests_snapshot_9d311d60.js"],"runtimeModuleIds":[64]}
+    {"otherChunks":["crates_pack-tests_tests_snapshot_fded73f7.js"],"runtimeModuleIds":[64]}
 ]);
 // Dummy runtime
