f46e1ddd434b854abdafa38d605c43ed57e7b97d
fireairforce <1344492820@qq.com>
2025-12-15 13:12:05 +0800

    chore: update test case

--- crates/pack-tests/tests/snapshot/static-assets/wasm/output/main.js
+++ crates/pack-tests/tests/snapshot/static-assets/wasm/output/main.js
@@ -1,5 +1,5 @@
 (globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([
     typeof document === "object" ? document.currentScript : undefined,
-    {"otherChunks":["crates_pack-tests_tests_snapshot_62f5dbc4.js"],"runtimeModuleIds":[0]}
+    {"otherChunks":["crates_pack-tests_tests_snapshot_4ff246a0.js"],"runtimeModuleIds":[0]}
 ]);
 // Dummy runtime
