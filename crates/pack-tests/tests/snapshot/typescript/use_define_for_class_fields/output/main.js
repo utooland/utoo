0df4fb8f724334f3432adc10055341766e754cb1
fireairforce <1344492820@qq.com>
2025-12-23 00:37:01 +0800

    fix(pack): fix async chunk name

---
+++ crates/pack-tests/tests/snapshot/typescript/use_define_for_class_fields/output/main.js
@@ -0,0 +1,5 @@
+(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([
+    typeof document === "object" ? document.currentScript : undefined,
+    {"otherChunks":["typescript_use_define_for_class_fields_input_index_ts_99061d86.js"],"runtimeModuleIds":["[project]/typescript/use_define_for_class_fields/input/index.ts [client] (ecmascript)"]}
+]);
+// Dummy runtime
---
+++ crates/pack-tests/tests/snapshot/typescript/use_define_for_class_fields/output/typescript_use_define_for_class_fields_input_index_ts_99061d86.js
@@ -0,0 +1,15 @@
+(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([typeof document === "object" ? document.currentScript : undefined,
+"[project]/typescript/use_define_for_class_fields/input/index.ts [client] (ecmascript)", ((__turbopack_context__) => {
+"use strict";
+
+class Base {
+    value;
+}
+__turbopack_context__.s([
+    "Base",
+    ()=>Base
+]);
+}),
+]);
+
+//# sourceMappingURL=typescript_use_define_for_class_fields_input_index_ts_99061d86.js.map
