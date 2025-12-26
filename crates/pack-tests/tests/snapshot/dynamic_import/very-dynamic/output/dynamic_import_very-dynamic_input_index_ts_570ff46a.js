4d6da28e7480a230a102372a4418ddcac9ddcbb5
fireairforce <1344492820@qq.com>
2025-12-26 15:41:28 +0800

    chore: add library snapshot test

--- crates/pack-tests/tests/snapshot/dynamic_import/very-dynamic/output/dynamic_import_very-dynamic_input_index_ts_570ff46a.js
+++ crates/pack-tests/tests/snapshot/dynamic_import/very-dynamic/output/dynamic_import_very-dynamic_input_index_ts_570ff46a.js
@@ -1,5 +1,5 @@
 (globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([typeof document === "object" ? document.currentScript : undefined,
-12, ((__turbopack_context__) => {
+67, ((__turbopack_context__) => {
 "use strict";
 
 async function importUrl(url) {
@@ -25,4 +25,4 @@ __turbopack_context__.s([
 }),
 ]);
 
-//# sourceMappingURL=dynamic_import_input_index_ts_403d7278.js.map
+//# sourceMappingURL=dynamic_import_very-dynamic_input_index_ts_570ff46a.js.map
