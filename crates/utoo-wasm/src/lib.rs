0a8de1e4fa37eb0017dd080dcae661e46d912f1d
xusd320 <xusd320@gmail.com>
2025-12-30 18:26:26 +0800

    perf(wasm): update tokio-fs-ext & opfs-project

--- crates/utoo-wasm/src/lib.rs
+++ crates/utoo-wasm/src/lib.rs
@@ -56,7 +56,7 @@ fn init_pack() {
 #[wasm_bindgen(js_name = "initLogFilter")]
 pub fn init_log_filter(mut filter: String) {
     if filter.is_empty() {
-        filter.push_str("pack_napi=info,pack_api=info,pack_core=info")
+        filter.push_str("utoo_wasm=info,pack_api=info,pack_core=info")
     }
     TRACING_INIT.call_once(|| {
         let filter_str = filter.clone();
