f496c39776cf69dba5525fb65a049831f5187656
fireairforce <1344492820@qq.com>
2026-01-06 23:28:56 +0800

    chore: add case for removeUnusedImports at sideEffects

--- crates/pack-tests/tests/snapshot/optimization/remove_unused_imports_reexport/output/input_121f988c.js
+++ crates/pack-tests/tests/snapshot/optimization/remove_unused_imports_reexport/output/input_121f988c.js
@@ -1,18 +1,18 @@
 (globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([typeof document === "object" ? document.currentScript : undefined,
-27, ((__turbopack_context__) => {
+"[project]/optimization/remove_unused_imports_reexport/input/wrapper.js [client] (ecmascript) <locals>", ((__turbopack_context__) => {
 "use strict";
 
 // Imports fnA from pkg and USES it
 // Re-exports fnB from pkg
-var __TURBOPACK__imported__module__97__ = __turbopack_context__.i(97);
+var __TURBOPACK__imported__module__$5b$project$5d2f$optimization$2f$remove_unused_imports_reexport$2f$input$2f$node_modules$2f$pkg$2f$a$2e$js__$5b$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/optimization/remove_unused_imports_reexport/input/node_modules/pkg/a.js [client] (ecmascript)");
 ;
 ;
 function useFnA() {
-    return (0, __TURBOPACK__imported__module__97__["fnA"])();
+    return (0, __TURBOPACK__imported__module__$5b$project$5d2f$optimization$2f$remove_unused_imports_reexport$2f$input$2f$node_modules$2f$pkg$2f$a$2e$js__$5b$client$5d$__$28$ecmascript$29$__["fnA"])();
 }
 __turbopack_context__.s([]);
 }),
-62, ((__turbopack_context__) => {
+"[project]/optimization/remove_unused_imports_reexport/input/node_modules/pkg/b.js [client] (ecmascript)", ((__turbopack_context__) => {
 "use strict";
 
 function fnB() {
@@ -24,15 +24,15 @@ __turbopack_context__.s([
     fnB
 ]);
 }),
-26, ((__turbopack_context__) => {
+"[project]/optimization/remove_unused_imports_reexport/input/index.js [client] (ecmascript)", ((__turbopack_context__) => {
 "use strict";
 
 // Entry only imports fnB, but wrapper.js uses fnA
 // BUG: fnA factory is removed because entry doesn't use it
-var __TURBOPACK__imported__module__27__ = __turbopack_context__.i(27);
-var __TURBOPACK__imported__module__62__ = __turbopack_context__.i(62);
+var __TURBOPACK__imported__module__$5b$project$5d2f$optimization$2f$remove_unused_imports_reexport$2f$input$2f$wrapper$2e$js__$5b$client$5d$__$28$ecmascript$29$__$3c$locals$3e$__ = __turbopack_context__.i("[project]/optimization/remove_unused_imports_reexport/input/wrapper.js [client] (ecmascript) <locals>");
+var __TURBOPACK__imported__module__$5b$project$5d2f$optimization$2f$remove_unused_imports_reexport$2f$input$2f$node_modules$2f$pkg$2f$b$2e$js__$5b$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/optimization/remove_unused_imports_reexport/input/node_modules/pkg/b.js [client] (ecmascript)");
 ;
-console.log((0, __TURBOPACK__imported__module__62__["fnB"])());
+console.log((0, __TURBOPACK__imported__module__$5b$project$5d2f$optimization$2f$remove_unused_imports_reexport$2f$input$2f$node_modules$2f$pkg$2f$b$2e$js__$5b$client$5d$__$28$ecmascript$29$__["fnB"])());
 __turbopack_context__.s([]);
 }),
 ]);
--- crates/pack-tests/tests/snapshot/optimization/remove_unused_imports_reexport/output/main.js
+++ crates/pack-tests/tests/snapshot/optimization/remove_unused_imports_reexport/output/main.js
@@ -1,5 +1,5 @@
 (globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([
     typeof document === "object" ? document.currentScript : undefined,
-    {"otherChunks":["input_121f988c.js"],"runtimeModuleIds":[26]}
+    {"otherChunks":["input_121f988c.js"],"runtimeModuleIds":["[project]/optimization/remove_unused_imports_reexport/input/index.js [client] (ecmascript)"]}
 ]);
 // Dummy runtime
