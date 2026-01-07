0060e6b415ab0381fdd67c51fdb5b036a94d0e8e
xusd320 <xusd320@gmail.com>
2026-01-07 15:03:23 +0800

    test(pack): add integration test

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -2,14 +2,10 @@ import React from "react";
 import Person from "../static/person.svg";
 import { foo } from "./foo.ts";
 import styles from "./index.module.less";
-import dataText from "./test.txt";
 
 export function App() {
   return (
     <>
-      <h1 className={styles.pre}>
-        App2 {foo} - HMR Test by {dataText}
-      </h1>
       <h1>React Version is: {React.version}</h1>
       <Person />
     </>
