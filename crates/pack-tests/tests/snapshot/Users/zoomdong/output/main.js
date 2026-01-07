eca43f11cf06061af42015429285cd0ae480f6fb
zoomdong <1344492820@qq.com>
2026-01-07 11:59:24 +0800

    chore: remove useless snapshot (#2493)

--- crates/pack-tests/tests/snapshot/Users/zoomdong/output/main.js
+++
@@ -1,5 +0,0 @@
-(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([
-    typeof document === "object" ? document.currentScript : undefined,
-    {"otherChunks":[],"runtimeModuleIds":[]}
-]);
-// Dummy runtime
