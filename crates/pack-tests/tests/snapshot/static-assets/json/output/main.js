a5e3992de929dd1bd3f72710b0ce9a2b201ccf4e
zoomdong <1344492820@qq.com>
2026-01-09 11:14:54 +0800

    fix(pack): output chunkFileName should not contains invalid charater (#2505)

---
+++ crates/pack-tests/tests/snapshot/static-assets/json/output/main.js
@@ -0,0 +1,5 @@
+(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([
+    typeof document === "object" ? document.currentScript : undefined,
+    {"otherChunks":["input_f4a57059.js"],"runtimeModuleIds":[59]}
+]);
+// Dummy runtime
