c467c0c4b6d2dd3af19e7b4c9280db7bb1845686
xusd320 <xusd320@gmail.com>
2026-01-21 15:12:20 +0800

    refactor(utoo-web): use unique hex magic numbers for message enums

--- packages/utoo-web/src/message.ts
+++ packages/utoo-web/src/message.ts
@@ -1,8 +1,15 @@
-export const HandShake = "__handshake__";
-export const Fork = "__fork__";
+export enum WorkerMessageType {
+  InitConnection = 0x574d0000, // "WM" in hex
+  RequestFork,
+}
+
+/** @deprecated use WorkerMessageType.InitConnection */
+export const HandShake = WorkerMessageType.InitConnection;
+/** @deprecated use WorkerMessageType.RequestFork */
+export const Fork = WorkerMessageType.RequestFork;
 
 export enum SWMessageType {
-  HandShake = 10000,
+  HandShake = 0x53570000, // "SW" in hex
   HeartbeatPing,
   HeartbeatPong,
 }
--- packages/utoo-web/src/worker.ts
+++ packages/utoo-web/src/worker.ts
@@ -1,5 +1,5 @@
 import * as comlink from "comlink";
-import { HandShake } from "./message";
+import { WorkerMessageType } from "./message";
 import { internalEndpoint } from "./project/InternalProject";
 
 declare let self: DedicatedWorkerGlobalScope;
@@ -8,7 +8,7 @@ const ConnectedPorts = new Set<MessagePort>();
 
 self.addEventListener("message", (e) => {
   const port = e.ports[0];
-  if (e.data === HandShake && !ConnectedPorts.has(port)) {
+  if (e.data === WorkerMessageType.InitConnection && !ConnectedPorts.has(port)) {
     comlink.expose(internalEndpoint, port);
     ConnectedPorts.add(port);
   }
