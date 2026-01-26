e937c07d14cf860387c8a129b2124ebc94f9bc45
xusd320 <xusd320@gmail.com>
2026-01-26 18:30:15 +0800

    feat(pack-cli): migrate pack-cli from oclif to citty and rename dist to cjs (#2543)

--- packages/pack-cli/src/commands/build.ts
+++ packages/pack-cli/src/commands/build.ts
@@ -1,20 +1,30 @@
-import { Command } from "@oclif/core";
 import * as utooPack from "@utoo/pack";
-import { commonFlags, resolveBuildOptions } from "../utils/common";
+import { defineCommand } from "citty";
+import { resolveBuildOptions } from "../utils/common";
 
-export default class Build extends Command {
-  static description = "utoopack build";
-  static examples = [
-    `<%= config.bin %> <%= command.id %> build --project .`,
-    `<%= config.bin %> <%= command.id %> build --project . --root ../..`,
-    `<%= config.bin %> <%= command.id %> build --webpack`,
-  ];
-  static flags = commonFlags;
-
-  async run(): Promise<void> {
-    const { flags } = await this.parse(Build);
-    const { projectOptions, projectPath, rootPath } =
-      resolveBuildOptions(flags);
+export default defineCommand({
+  meta: {
+    name: "build",
+    description: "utoopack build",
+  },
+  args: {
+    project: {
+      type: "string",
+      alias: "p",
+      description: "Set the project path",
+    },
+    root: {
+      type: "string",
+      alias: "r",
+      description: "Set the root path",
+    },
+    webpack: {
+      type: "boolean",
+      description: "Enable webpack mode",
+    },
+  },
+  async run({ args }) {
+    const { projectOptions, projectPath, rootPath } = resolveBuildOptions(args);
     await utooPack.build(projectOptions, projectPath, rootPath);
-  }
-}
+  },
+});
