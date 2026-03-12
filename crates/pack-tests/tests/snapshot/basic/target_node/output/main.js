9fad598da50dd7572b4398729e4554059b2a0c4c
xusd320 <xusd320@gmail.com>
2026-03-12 10:55:52 +0800

    fix(test): update snapshots

--- crates/pack-tests/tests/snapshot/basic/target_node/output/main.js
+++ crates/pack-tests/tests/snapshot/basic/target_node/output/main.js
@@ -1,4 +1,4 @@
 var R=require("./_turbopack__runtime.js")("main.js")
-R.c("_project__basic_target_node_input_ae63d396.js")
+R.c("_root-of-the-server___1e3d7703.js")
 R.m("[project]/basic/target_node/input/index.ts [server] (ecmascript)")
 module.exports=R.m("[project]/basic/target_node/input/index.ts [server] (ecmascript)").exports
