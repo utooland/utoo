224d2be8927d82ba42a9f3de6d1d5884bfc2a064
fireairforce <1344492820@qq.com>
2026-03-15 23:16:25 +0800

    fix(pack): merged alias lost module factory with missing import type

--- crates/pack-tests/tests/snapshot/concatenate_modules/duplicate-imports/output/input_9640720b.js
+++ crates/pack-tests/tests/snapshot/concatenate_modules/duplicate-imports/output/input_9640720b.js
@@ -1,9 +1,9 @@
 (globalThis["TURBOPACK"] || (globalThis["TURBOPACK"] = [])).push([typeof document === "object" ? document.currentScript : undefined,
-91, ((__turbopack_context__, module, exports) => {
+"[project]/concatenate_modules/duplicate-imports/input/shared.js [client] (ecmascript)", ((__turbopack_context__, module, exports) => {
 
 module.exports = 'shared';
 }),
-80, ((__turbopack_context__) => {
+"[project]/concatenate_modules/duplicate-imports/input/index.js [client] (ecmascript)", ((__turbopack_context__) => {
 "use strict";
 
 // MERGED MODULE: [project]/concatenate_modules/duplicate-imports/input/index.js [client] (ecmascript)
@@ -14,19 +14,19 @@ module.exports = 'shared';
 ;
 // MERGED MODULE: [project]/concatenate_modules/duplicate-imports/input/a.js [client] (ecmascript)
 ;
-var __TURBOPACK__imported__module__91__ = __turbopack_context__.i(91);
+var __TURBOPACK__imported__module__$5b$project$5d2f$concatenate_modules$2f$duplicate$2d$imports$2f$input$2f$shared$2e$js__$5b$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/concatenate_modules/duplicate-imports/input/shared.js [client] (ecmascript)");
 ;
-console.log('a', __TURBOPACK__imported__module__91__["default"]);
-var __TURBOPACK__imported__module__91__1 = __TURBOPACK__imported__module__91__;
-;
-;
-console.log('a', __TURBOPACK__imported__module__91__1["default"]);
-var __TURBOPACK__imported__module__91__2 = __TURBOPACK__imported__module__91__;
+console.log('a', __TURBOPACK__imported__module__$5b$project$5d2f$concatenate_modules$2f$duplicate$2d$imports$2f$input$2f$shared$2e$js__$5b$client$5d$__$28$ecmascript$29$__["default"]);
+var __TURBOPACK__imported__module__$5b$project$5d2f$concatenate_modules$2f$duplicate$2d$imports$2f$input$2f$shared$2e$js__$5b$client$5d$__$28$ecmascript$29$__1 = __TURBOPACK__imported__module__$5b$project$5d2f$concatenate_modules$2f$duplicate$2d$imports$2f$input$2f$shared$2e$js__$5b$client$5d$__$28$ecmascript$29$__;
 ;
 ;
-console.log('c', __TURBOPACK__imported__module__91__2["default"]);
+console.log('a', __TURBOPACK__imported__module__$5b$project$5d2f$concatenate_modules$2f$duplicate$2d$imports$2f$input$2f$shared$2e$js__$5b$client$5d$__$28$ecmascript$29$__1["default"]);
+var __TURBOPACK__imported__module__$5b$project$5d2f$concatenate_modules$2f$duplicate$2d$imports$2f$input$2f$shared$2e$js__$5b$client$5d$__$28$ecmascript$29$__2 = __TURBOPACK__imported__module__$5b$project$5d2f$concatenate_modules$2f$duplicate$2d$imports$2f$input$2f$shared$2e$js__$5b$client$5d$__$28$ecmascript$29$__;
 ;
-__turbopack_context__.s([], 80);
+;
+console.log('c', __TURBOPACK__imported__module__$5b$project$5d2f$concatenate_modules$2f$duplicate$2d$imports$2f$input$2f$shared$2e$js__$5b$client$5d$__$28$ecmascript$29$__2["default"]);
+;
+__turbopack_context__.s([], "[project]/concatenate_modules/duplicate-imports/input/index.js [client] (ecmascript)");
 }),
 ]);
 
