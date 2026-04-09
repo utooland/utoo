aa727361316361ca00a6bd0dfba1afbcc923e4d9
zoomdong <1344492820@qq.com>
2026-04-09 00:21:12 -0700

    chore(test): add enum with as syntax test case (#2771)

--- crates/pack-tests/tests/snapshot/typescript/verbatim_module_syntax/output/input_index_tsx_0a80628f.js
+++ crates/pack-tests/tests/snapshot/typescript/verbatim_module_syntax/output/input_index_tsx_0a80628f.js
@@ -6,8 +6,16 @@
 const App = ()=>{
     return /*#__PURE__*/ React.createElement("div", null, "Verbatim module syntax Test Case");
 };
+var RefType = /*#__PURE__*/ function(RefType) {
+    RefType["property"] = "11";
+    RefType["event"] = "22";
+    return RefType;
+}({});
+console.log("11", "22");
 var __TURBOPACK__default__export__ = App;
 __turbopack_context__.s([
+    "RefType",
+    ()=>RefType,
     "default",
     0,
     __TURBOPACK__default__export__
