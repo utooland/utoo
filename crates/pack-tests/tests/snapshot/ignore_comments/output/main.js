0329f39d046db2bf823fcbdffda9fad1dd617d08
fireairforce <1344492820@qq.com>
2026-04-14 23:28:23 +0800

    fix(pack): recognize turbopack magic comments

---
+++ crates/pack-tests/tests/snapshot/ignore_comments/output/main.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK"] || (globalThis["TURBOPACK"] = [])).push([
+    typeof document === "object" ? document.currentScript : undefined,
+    {"otherChunks":["input_acb336a8.js"],"runtimeModuleIds":["[project]/ignore_comments/input/index.js [client] (ecmascript)"]}
+]);
+// Dummy runtime
