75d49a608bd051ec976b7cd4720475fb21a27194
killa <killa07071201@gmail.com>
2026-05-05 14:43:48 +0800

    fix(pack): resolve node exports for server bundles

---
+++ crates/pack-tests/tests/snapshot/target_node/cjs_esm_interop/output/main.js
@@ -0,0 +1,4 @@
+var R=require("./_turbopack__runtime.js")("main.js")
+R.c("_project__target_node_cjs_esm_interop_input_80f4d969.js")
+R.m("[project]/target_node/cjs_esm_interop/input/index.js [server] (ecmascript)")
+module.exports=R.m("[project]/target_node/cjs_esm_interop/input/index.js [server] (ecmascript)").exports
