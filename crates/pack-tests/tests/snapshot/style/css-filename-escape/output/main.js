0436ebd39835e7c07baf426949690747cf2546e0
fireairforce <1344492820@qq.com>
2026-05-07 14:22:29 +0800

    fix(pack): add & as escape chunk filename character

---
+++ crates/pack-tests/tests/snapshot/style/css-filename-escape/output/main.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK"] || (globalThis["TURBOPACK"] = [])).push([
+    typeof document === "object" ? document.currentScript : undefined,
+    {"otherChunks":["input_RuleEdit_Vidw_less_29d5bafe.css","input_index_d45174ac.js"],"runtimeModuleIds":["[project]/style/css-filename-escape/input/index.js [client] (ecmascript)"]}
+]);
+// Dummy runtime
