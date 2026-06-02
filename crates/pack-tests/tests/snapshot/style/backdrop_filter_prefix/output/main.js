a5a4f7e071b311a4d164951f12cabc7679893d44
fireairforce <1344492820@qq.com>
2026-06-02 16:19:02 +0800

    fix(pack): lightningcss perserve -webkit- prefix after backdrop-filter property

---
+++ crates/pack-tests/tests/snapshot/style/backdrop_filter_prefix/output/main.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK"] || (globalThis["TURBOPACK"] = [])).push([
+    typeof document === "object" ? document.currentScript : undefined,
+    {"otherChunks":["input_navbar_ea32f930.css","input_index_6ad5b8de.js"],"runtimeModuleIds":["[project]/style/backdrop_filter_prefix/input/index.js [client] (ecmascript)"]}
+]);
+// Dummy runtime
