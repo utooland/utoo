f1b33451c7be2dc062d204f21f7402e9c0a19359
zoomdong <1344492820@qq.com>
2026-06-10 17:04:51 +0800

    fix(pack): respect local postcss config file when enable postcssTransform (#3136)
    * fix(pack): respect local postcss config file when enable postCsstransform
    
    * chore: update snapshot

---
+++ crates/pack-tests/tests/snapshot/style/local_postcss_config/input/index.js
@@ -0,0 +1,3 @@
+import "./style.css";
+
+console.log("project postcss config test");
---
+++ crates/pack-tests/tests/snapshot/style/local_postcss_config/output/main.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK"] || (globalThis["TURBOPACK"] = [])).push([
+    typeof document === "object" ? document.currentScript : undefined,
+    {"otherChunks":["input_style_19354859.css","input_index_f9cdb69e.js"],"runtimeModuleIds":["[project]/style/local_postcss_config/input/index.js [client] (ecmascript)"]}
+]);
+// Dummy runtime
