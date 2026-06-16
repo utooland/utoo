82d9fca11fa7c2c76d4aa732905919941a6c738b
fireairforce <1344492820@qq.com>
2026-06-16 11:15:41 +0800

    feat(pack): support config cssModules like localIdentName

---
+++ crates/pack-tests/tests/snapshot/style/css_modules_local_ident/output/input_6ba844a1.js
@@ -0,0 +1,17 @@
+(globalThis["TURBOPACK"] || (globalThis["TURBOPACK"] = [])).push([typeof document === "object" ? document.currentScript : undefined,
+"[project]/style/css_modules_local_ident/input/style.less?modules [client] (css module)", ((__turbopack_context__) => {
+
+__turbopack_context__.v({
+  "primaryButton": "primaryButton___cHsHq",
+  "title": "title___cHsHq",
+});
+}),
+"[project]/style/css_modules_local_ident/input/index.ts [client] (ecmascript)", ((__turbopack_context__) => {
+"use strict";
+
+var __TURBOPACK__imported__module__$5b$project$5d2f$style$2f$css_modules_local_ident$2f$input$2f$style$2e$less$3f$modules__$5b$client$5d$__$28$css__module$29$__ = __turbopack_context__.i("[project]/style/css_modules_local_ident/input/style.less?modules [client] (css module)");
+;
+console.log(__TURBOPACK__imported__module__$5b$project$5d2f$style$2f$css_modules_local_ident$2f$input$2f$style$2e$less$3f$modules__$5b$client$5d$__$28$css__module$29$__["default"].title, __TURBOPACK__imported__module__$5b$project$5d2f$style$2f$css_modules_local_ident$2f$input$2f$style$2e$less$3f$modules__$5b$client$5d$__$28$css__module$29$__["default"].primaryButton);
+__turbopack_context__.s([]);
+}),
+]);
