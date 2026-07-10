6e7dc8afaea586078a078f679a833f8effdcf555
fireairforce <1344492820@qq.com>
2026-07-10 17:23:40 +0800

    chore(pack): add @import inlineCss postcss test case

--- crates/pack-tests/tests/snapshot/style/inline_css_postcss/output/_root-of-the-server___60743e00.js
+++ crates/pack-tests/tests/snapshot/style/inline_css_postcss/output/_root-of-the-server___60743e00.js
@@ -226,7 +226,7 @@ module.exports = function(list, options) {
 
 var __TURBOPACK__imported__module__$5b40$utoo$2f$pack$2d$runtime$5d2f$inline_css$2f$injectStylesIntoStyleTag$2e$js__$5b$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[@utoo/pack-runtime]/inline_css/injectStylesIntoStyleTag.js [client] (ecmascript)");
 ;
-var content = ".inline-postcss {\n  width: 2rem;\n  height: 1rem;\n  margin: .5rem .25rem;\n}\n";
+var content = "@import \"./test.css\";\n\n.inline-postcss {\n  width: 2rem;\n  height: 1rem;\n  margin: .5rem .25rem;\n}\n";
 var options = {};
 options.insert = "head";
 options.singleton = false;
