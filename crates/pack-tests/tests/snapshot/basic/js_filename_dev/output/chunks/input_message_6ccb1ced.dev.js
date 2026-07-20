60adbef7d9ffd35589317e3204f32772c59c9468
fireairforce <1344492820@qq.com>
2026-07-21 01:23:15 +0800

    test(pack): update dynamic HMR snapshot

--- crates/pack-tests/tests/snapshot/basic/js_filename_dev/output/chunks/input_message_6ccb1ced.dev.js
+++ crates/pack-tests/tests/snapshot/basic/js_filename_dev/output/chunks/input_message_6ccb1ced.dev.js
@@ -3,7 +3,8 @@
 
 __turbopack_context__.v((parentImport) => {
     return Promise.all([
-  "chunks/input_message_7d12845b.dev.js"
+  "chunks/input_message_7d12845b.dev.js",
+  "chunks/input_message_807614c3.dev.js"
 ].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
         return parentImport("[project]/basic/js_filename_dev/input/message.js [client] (ecmascript)");
     });
