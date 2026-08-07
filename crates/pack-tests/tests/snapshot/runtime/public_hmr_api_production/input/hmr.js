cbaa1f514bff2f4d123358ca2f6af401296fa8ac
fireairforce <1344492820@qq.com>
2026-08-07 18:21:12 +0800

    test(pack): simplify public HMR coverage

--- crates/pack-tests/tests/snapshot/runtime/public_hmr_api_production/input/hmr.js
+++
@@ -1,9 +0,0 @@
-if (import.meta.turbopackHot !== undefined) {
-  throw new Error("import.meta.turbopackHot should be disabled");
-}
-
-if (module.hot !== undefined) {
-  throw new Error("module.hot should be disabled");
-}
-
-export const value = "initial";
